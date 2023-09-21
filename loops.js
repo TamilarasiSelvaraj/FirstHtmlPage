85f2bedefed2de78dcd413573ec9a1582fe57bed
unknown <seethatamilarasi@gmail.com>
2023-09-21 14:34:33 +0530

    changes to be added

---
+++ loops.js
@@ -0,0 +1,20 @@
+
+   // For loop  Use Inline variable
+   for (let i = 1; i <= 5; i++){
+   document.getElementById("loop").innerHTML=("Numbers#"+i+"<br>");
+   console.log("Number#"+i);
+   }
+//For loop 
+   /*let fruits=["apple","orrange","mango","grapes"];
+
+   for (let i = 0;i<fruits.length;i++){
+    console.log (i,fruits[i]);
+   }*/
+   //while loop
+   let days=["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
+   let a=0;
+   while ( a<days.length){
+   
+    console.log(a,days[a]);
+    a++;
+   }
