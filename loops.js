184724d97f5604da4c846a60c61d060dbffacf6a
unknown <seethatamilarasi@gmail.com>
2023-09-21 14:58:01 +0530

    For-in loop added

--- loops.js
+++ loops.js
@@ -18,3 +18,19 @@
     console.log(a,days[a]);
     a++;
    }
+   //for in loop( use object and arrays)
+// for in loop use object
+   let object = {
+    name:"Tamil",
+    gender:"female",
+    id:3,
+    rang:5,
+};
+for (let instance in object){
+    console.log(instance+":"+object[instance]);
+}
+//for in loop use arrays
+let languages = ["Tamil","Hindi","Malayalam","Telungu"];
+for (let lang in languages){
+    console.log(lang,languages[lang]);
+}
